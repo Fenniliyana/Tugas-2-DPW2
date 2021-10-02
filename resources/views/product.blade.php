8844fd36f4b191aa51129a1be700595335e883b6
Fenniliyana <91305370+Fenniliyana@users.noreply.github.com>
2021-10-02 21:16:06 +0700

    Tugas 2 DPW2

---
+++ resources/views/product.blade.php
@@ -0,0 +1,455 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <!-- Basic -->
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Mobile Metas -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+
+  <!-- Site Metas -->
+  <meta name="keywords" content="" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+
+  <title>GymFen</title>
+
+  <!-- bootstrap core css -->
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+
+  <!-- fonts style -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap" rel="stylesheet" />
+  <!-- slick slider -->
+  <!-- font awesome style -->
+  <link href="css/font-awesome.min.css" rel="stylesheet" />
+  <!-- Custom styles for this template -->
+  <link href="css/style.css" rel="stylesheet" />
+  <!-- responsive style -->
+  <link href="css/responsive.css" rel="stylesheet" />
+</head>
+
+<body class="sub_page">
+  <div class="hero_area">
+    <div class="hero_bg_box">
+      <img src="images/hero-bg.jpg" alt="">
+    </div>
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <img src="images/logo.png" alt="">
+            <span>
+              GymFen
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="d-flex mx-auto flex-column flex-lg-row align-items-center">
+              <ul class="navbar-nav  ">
+                <li class="nav-item ">
+                  <a class="nav-link" href="/">Home </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/about"> About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/product">Products</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/why">Why Us</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/testimonial">Testimonial</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/login">login</a>
+                </li>
+              </ul>
+            </div>
+            <div class="quote_btn-container">
+              <a href="">
+                <i class="fa fa-user" aria-hidden="true"></i>
+              </a>
+              <a href="">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+              </a>
+              <form class="form-inline">
+                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+  </div>
+
+  <!-- product section -->
+
+  <section class="product_section layout_padding">
+    <div class="container">
+      <div class="heading_container heading_center">
+        <h2>
+          New products
+        </h2>
+      </div>
+      <div class="product_container">
+        <div class="row">
+          <div class="col-sm-6 col-lg-4 ">
+            <div class="box">
+              <a href="" class="p_cart">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+              </a>
+              <div class="img-box">
+                <img src="images/p1.png" alt="">
+              </div>
+              <div class="detail-box">
+                <span class="p_rating">
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                </span>
+                <a href="" class="p_name">
+                  Electric Treadmill OB-1061 Ergonomic Design for Home Use
+                </a>
+                <h6 class="p_price">
+                  <span class="new_price">
+                    Rp 9,765,000
+                  </span>
+                  <span class="old_price">
+                   Rp 10,900,000
+                  </span>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 ">
+            <div class="box">
+              <a href="" class="p_cart">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+              </a>
+              <div class="img-box">
+                <img src="images/p2.png" alt="">
+              </div>
+              <div class="detail-box">
+                <span class="p_rating">
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                </span>
+                <a href="" class="p_name">
+                 DOMYOS Rowing machine 100 8527501
+                </a>
+                <h6 class="p_price">
+                  <span class="new_price">
+                   Rp3.550.000
+                  </span>
+                  <span class="old_price">
+                   Rp5.550.000
+                  </span>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 ">
+            <div class="box">
+              <a href="" class="p_cart">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+              </a>
+              <div class="img-box">
+                <img src="images/p3.png" alt="">
+              </div>
+              <div class="detail-box">
+                <span class="p_rating">
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                </span>
+                <a href="" class="p_name">
+                  Cardio Alat Olahraga Masa Kini
+                </a>
+                <h6 class="p_price">
+                  <span class="new_price">
+                  Rp2.200.000
+                  </span>
+                  <span class="old_price">
+                   Rp4.100.000
+                  </span>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 ">
+            <div class="box">
+              <a href="" class="p_cart">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+              </a>
+              <div class="img-box">
+                <img src="images/p4.png" alt="">
+              </div>
+              <div class="detail-box">
+                <span class="p_rating">
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                </span>
+                <a href="" class="p_name">
+                  Cardio Twister 
+                </a>
+                <h6 class="p_price">
+                  <span class="new_price">
+                  Rp1.200.000
+                  </span>
+                  <span class="old_price">
+                  Rp2.950.000
+                  </span>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 ">
+            <div class="box">
+              <a href="" class="p_cart">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+              </a>
+              <div class="img-box">
+                <img src="images/p5.png" alt="">
+              </div>
+              <div class="detail-box">
+                <span class="p_rating">
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                </span>
+                <a href="" class="p_name">
+                 Lat Pull Down Rowing DP
+                </a>
+                <h6 class="p_price">
+                  <span class="new_price">
+                   Rp14.250.000
+                  </span>
+                  <span class="old_price">
+                  Rp15.250.000
+                  </span>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm-6 col-lg-4 ">
+            <div class="box">
+              <a href="" class="p_cart">
+                <i class="fa fa-cart-plus" aria-hidden="true"></i>
+              </a>
+              <div class="img-box">
+                <img src="images/p6.png" alt="">
+              </div>
+              <div class="detail-box">
+                <span class="p_rating">
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                  <a href="">
+                    <i class="fa fa-star" aria-hidden="true"></i>
+                  </a>
+                </span>
+                <a href="" class="p_name">
+                  Bench Press OB-204 for Home Use Trainee
+                </a>
+                <h6 class="p_price">
+                  <span class="new_price">
+                    Rp 6,000,000
+                  </span>
+                  <span class="old_price">
+                    Rp 8,000,000
+                  </span>
+                </h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="btn-box">
+        <a href="">
+          See More
+        </a>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- end product section -->
+
+  <!-- info section -->
+
+  <section class="info_section ">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-9 mx-auto">
+
+          <div class="info_form">
+            <h4>
+              Subscribe To Our Newsletter
+            </h4>
+            <form action="">
+              <input type="mail" placeholder="Enter your email...">
+              <button type="submit">
+                Subscribe
+              </button>
+            </form>
+          </div>
+
+          <h4>
+            Contact Us
+          </h4>
+          <div class="contact_items">
+            <a href="">
+              <div class="img-box">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+              </div>
+              <h6>
+                ketapang, jl kartini no.21,Kalbar
+              </h6>
+            </a>
+            <a href="">
+              <div class="img-box">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+              </div>
+              <h6>
+                fenni@gmail.com
+              </h6>
+            </a>
+            <a href="">
+              <div class="img-box">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+              </div>
+              <h6>
+                (+628 5023856288)
+              </h6>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="box">
+        <div class="info_social">
+          <div>
+            <a href="">
+              <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+          </div>
+          <div>
+            <a href="">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+          </div>
+          <div>
+            <a href="">
+              <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+          </div>
+          <div>
+            <a href="">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end info section -->
+  <!-- footer section -->
+  <footer class="footer_section">
+    <p>
+      &copy; <span id="displayYear"></span> By
+      <a href="https://html.design/">Fenniliyana</a>
+    </p>
+  </footer>
+  <!-- footer section -->
+  <!-- jQery -->
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <!-- bootstrap js -->
+  <script src="js/bootstrap.js"></script>
+  <!-- custom js -->
+  <script src="js/custom.js"></script>
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Basic -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <!-- Site Metas -->
+    <link rel="icon" href="{{url('assets')}}/images/fevicon/fevicon.png" type="image/gif" />
+    <meta name="keywords" content="" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+
+    <link href="{{url('system')}}/resources/css/login/style.css" rel="stylesheet" />
+
+</head>
+<body>
+    <form action="index.html" class="box" method="post">
+        <h1>Login</h1>
+        <input type="text" name="" placeholder="Username">
+        <input type="password" name="" placeholder="Password">
+        <input type="submit" name="" value="Login">
+        <p class="forget">Forget Password ? <a href="/" target="_blank">Klik Disini</a></p>
+    </form> 
+</body>
+</html>
